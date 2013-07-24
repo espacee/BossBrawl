fa08f4248a203b93ed5a77e3f89b327dfc25b6d2
Mika Attila <radiantstatue@gmail.com>
2013-07-25 00:55:59 +0200

    MenuState: Center menu buttons on screen

--- src/MenuState.cpp
+++ src/MenuState.cpp
@@ -2,25 +2,28 @@
 
 #include "graphics.hpp"
 #include "stateDriver.hpp"
+#include "config.hpp"
 
 void MenuState::onSet()
 {
     graphics::window.setTitle("menu");
     graphics::window.setView(graphics::window.getDefaultView());
 
+    int hc = config::windowWidth / 2, vc = config::windowHeight / 2;
+
     playButton.setText("Play");
     playButton.setSize(200,40);
-    playButton.setPosition(50,50);
+    playButton.setPosition(hc - 100, vc - 70);
 
     editorButton.setText("Editor");
     editorButton.setSize(200,40);
-    editorButton.setPosition(50,100);
+    editorButton.setPosition(hc - 100, vc - 20);
     editorButton.setColorScheme(sf::Color(240,140,60),sf::Color(255,215,90),sf::Color(170,85,60));
     editorButton.setTextColor(sf::Color(80,50,50));
 
     quitButton.setText("Quit");
     quitButton.setSize(200,40);
-    quitButton.setPosition(50,150);
+    quitButton.setPosition(hc - 100, vc + 30);
     quitButton.setColorScheme(sf::Color(240,80,130),sf::Color(250,150,180),sf::Color(200,15,50));
     quitButton.setTextColor(sf::Color(50,0,75));
 }
