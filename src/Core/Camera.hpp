7a6f594f49a284493e1f794e9943bd863bbeb8bb
Mika Attila <radiantstatue@gmail.com>
2013-07-25 21:30:57 +0200

    Core/Camera: Update documentation

--- src/Core/Camera.hpp
+++ src/Core/Camera.hpp
@@ -4,13 +4,13 @@
 #include <SFML/Graphics.hpp>
 
 /**
- * @brief A camera that follows a target point with a certain speed
+ * @brief A camera that can smoothly move to a target point with a set speed
  */
 class Camera
 {
 public:
     /**
-     * @brief Construct a camera with given size
+     * @brief Construct a camera with an optional size and target point
      * @param size The view area
      */
     Camera(const sf::Vector2f& size = sf::Vector2f(), const sf::Vector2f& targetPoint = sf::Vector2f());
@@ -22,19 +22,19 @@ public:
     void apply(sf::RenderTarget& renderTarget);
 
     /**
-     * @brief Set the target the camera should follow
-     * @param point The point the camera should follow
+     * @brief Set the target point the camera should move to
+     * @param point The point the camera should move to
      */
     void setTarget(const sf::Vector2f& point);
 
     /**
-     * @brief Move the target point the camera should follow by an offset
+     * @brief Move the target point the camera should move to by an offset
      * @param offset Offset to move the point by
      */
     void moveTarget(const sf::Vector2f& offset);
 
     /**
-     * @brief Get the target point the camera is following
+     * @brief Get the target point the camera is moving to
      * @return The target point
      */
     sf::Vector2f getTarget() const;
@@ -46,11 +46,11 @@ public:
     sf::View getView() const;
 
     /**
-     * @brief Set the speed the camera should follow the target at
-     * @param speed The speed the camera should follow the target at.
+     * @brief Set the speed at which the camera should move towards the target
+     * @param speed The speed at which the camera should move towards the target.
      *              It can be in the range [0..1] where
      *              0 means the camera won't move at all and
-     *              1 means the camera moves to the target position instantly
+     *              1 means the camera arrives at the target position instantly
      *
      *              If the speed is not within the valid range, it gets clamped.
      */
@@ -59,7 +59,7 @@ public:
     /**
      * @brief Update the position of the camera
      *
-     * The camera moves in the direction of the target at the given speed
+     * Call this function to actually move the camera
      */
     void update();
 
