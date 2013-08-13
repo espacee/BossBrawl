1830ceb0e4e34911ea36cc80ef2c24050a9b2be0
Layl <layldupont@gmail.com>
2013-08-14 01:11:05 +0200

    Basic editor layout

--- src/Editor/editorWindow.cpp
+++ src/Editor/editorWindow.cpp
@@ -1,7 +1,56 @@
 #include "editorWindow.h"
 
-Window::Window(QWidget *parent)
-    : QWidget(parent)
+Window::Window(QWidget *parent) : QWidget(parent)
+{
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowMinimizeButtonHint);
+    setGeometry(QApplication::desktop()->availableGeometry());
+    setWindowTitle("BossBrawl-Editor");
+
+    menuBarHeight=50;
+    globalPadding=5;
+    toolBarWidth=40;
+    topBarHeight=30;
+    rightPanelWidth=200;
+    botBarHeight=20;
+
+    toolBar = new QWidget(this);
+    toolBar->setGeometry(globalPadding,
+                         menuBarHeight,
+                         toolBarWidth,
+                         height()-menuBarHeight-globalPadding);
+
+    rightPanel = new QWidget(this);
+    rightPanel->setGeometry(width()-globalPadding-rightPanelWidth,
+                            menuBarHeight,
+                            rightPanelWidth,
+                            height()-menuBarHeight-botBarHeight-globalPadding);
+
+    topBar = new QWidget(this);
+    topBar->setGeometry(globalPadding+toolBarWidth,
+                        menuBarHeight,
+                        width()-toolBarWidth-rightPanelWidth-globalPadding*2,
+                        topBarHeight);
+
+    botBar = new QWidget(this);
+    botBar->setGeometry(globalPadding+toolBarWidth,
+                        height()-globalPadding-botBarHeight,
+                        width()-toolBarWidth-globalPadding*2,
+                        botBarHeight);
+
+    centralWidget = new QWidget(this);
+    centralWidget->setGeometry(toolBar->x()+toolBar->width(),
+                               topBar->y()+topBar->height(),
+                               width()-toolBar->x()-toolBar->width()-rightPanelWidth-globalPadding,
+                               height()-menuBarHeight-topBarHeight-globalPadding-botBarHeight);
+
+    toolBar->setStyleSheet("background-color:black;");
+    rightPanel->setStyleSheet("background-color:blue;");
+    topBar->setStyleSheet("background-color:red;");
+    botBar->setStyleSheet("background-color:yellow;");
+    centralWidget->setStyleSheet("background-color:green;");
+}
+
+void Window::showEvent(QShowEvent*)
 {
 }
 
--- src/Editor/editorWindow.h
+++ src/Editor/editorWindow.h
@@ -2,6 +2,8 @@
 #define WINDOW_H
 
 #include <QWidget>
+#include <QApplication>
+#include <QDesktopWidget>
 
 class Window : public QWidget
 {
@@ -11,7 +13,11 @@ public:
     Window(QWidget *parent = 0);
     ~Window();
 
+    void showEvent(QShowEvent*);
+
 private:
+    int menuBarHeight, globalPadding, toolBarWidth, topBarHeight, rightPanelWidth, botBarHeight;
+    QWidget *toolBar, *topBar, *rightPanel, *botBar, *centralWidget;
 
 };
 
