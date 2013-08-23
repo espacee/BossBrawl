04d6ce793a166522cb6550cdbe1c4d8f02a7c527
Ryan Fallah <fallahnea@gmail.com>
2013-08-23 19:49:04 +0200

    fixed gravity bug

--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -8,9 +8,9 @@ Player::Player()
 void Player::onSet()
 {
     health = 100;
-    moveSpeed = 1.0f;
+    moveSpeed = 10.0f;
     jumpSpeed = 10.0f;
-    groundHeight = 80;
+    groundHeight = 160;
     velocity = sf::Vector2f(0,0);
 
     playerTexture.loadFromFile("res/img/GAME/Player.png");
@@ -27,34 +27,31 @@ void Player::display(sf::RenderWindow &target)
 }
 void Player::playerControls()
 {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
         velocity.y = -jumpSpeed;
-    }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-        velocity.x = moveSpeed;
-
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
         velocity.x = -moveSpeed;
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+        velocity.x = moveSpeed;
     }
     else
         velocity.x = 0;
 
 
-
-if(playerSprite.getPosition().y < groundHeight || velocity.y < 0  )
+if(playerSprite.getPosition().y + playerTexture.getSize().y < groundHeight || velocity.y < 0  )
 {
 velocity.y += gravity;
 }
 else
 {
-playerSprite.setPosition(playerSprite.getPosition().x, groundHeight);
+playerSprite.setPosition(playerSprite.getPosition().x, groundHeight -playerTexture.getSize().y );
 velocity.y = 0;
 }
 
 
-
-    playerSprite.move(velocity.x, velocity.y);
-
+playerSprite.move(velocity.x, velocity.y);
 
 }
+
