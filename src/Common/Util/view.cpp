1af3020164de0a12e2b8e337c296a64cb7ad0c86
Mika Attila <radiantstatue@gmail.com>
2013-11-20 11:32:36 +0100

    util: Remove unused version of moveViewTowardsPoint

--- src/Common/Util/view.cpp
+++ src/Common/Util/view.cpp
@@ -1,13 +1,7 @@
 #include "Util/view.hpp"
 
-void moveViewTowardsPoint(sf::View& view, float x, float y, float speed)
+void moveViewTowardsPoint(sf::View& view, sf::Vector2f point, float speed)
 {
-    view.setCenter((int)(view.getCenter().x + (x - view.getCenter().x) * speed),
-                   (int)(view.getCenter().y + (y - view.getCenter().y) * speed));
-}
-
-void moveViewTowardsPoint(sf::View& view, sf::Vector2f pos, float speed)
-{
-    view.setCenter((int)(view.getCenter().x + (pos.x - view.getCenter().x) * speed),
-                   (int)(view.getCenter().y + (pos.y - view.getCenter().y) * speed));
+    view.setCenter((int)(view.getCenter().x + (point.x - view.getCenter().x) * speed),
+                   (int)(view.getCenter().y + (point.y - view.getCenter().y) * speed));
 }
