c335b1dc8be894675f05641f0ec4cb40b587f924
Mika Attila <radiantstatue@gmail.com>
2013-11-21 19:13:07 +0100

    Editor/maps: Make current() implementation use a vector

--- src/Editor/maps.cpp
+++ src/Editor/maps.cpp
@@ -1,11 +1,19 @@
 #include "maps.hpp"
+#include <vector>
 
 namespace maps {
 
+std::vector<TileMap> m_tilemaps;
+int m_active = 0;
+
 TileMap& current()
 {
-    static TileMap map;
-    return map;
+    static bool inited;
+    if (!inited) {
+        m_tilemaps.emplace_back();
+        inited = true;
+    }
+    return m_tilemaps[m_active];
 }
 
 }
