032005d5c18c5637bf626ba538aec43ab9abe16d
Mika Attila <radiantstatue@gmail.com>
2013-11-23 17:50:13 +0100

    Editor/ToolbarWidget: Move image path definitions into constructor, add some more documentation

--- src/Editor/ToolbarWidget.cpp
+++ src/Editor/ToolbarWidget.cpp
@@ -1,32 +1,30 @@
 #include "ToolbarWidget.hpp"
 #include <QDebug>
 
-namespace {
-
-const char *images[TOOL_COUNT] {
-    "res/img/GUI/pointer.png",
-    "res/img/GUI/pen.png",
-    "res/img/GUI/randomPen.png",
-    "res/img/GUI/patternBrush.png",
-    "res/img/GUI/eraser.png",
-    "res/img/GUI/fillShape.png",
-    "res/img/GUI/selectArea.png",
-    "res/img/GUI/arrow.png",
-    "res/img/GUI/entity.png",
-    "res/img/GUI/object.png",
-    "res/img/GUI/hand.png",
-    "res/img/GUI/zoom.png"
-};
-
-} // End of anonymous namespace
-
 ToolbarWidget::ToolbarWidget(QWidget *parent_) :
     QWidget(parent_)
 {
+    // Map an image file for each tool to be used as its button's icon
+    const char *const images[TOOL_COUNT] {
+        "res/img/GUI/pointer.png",
+        "res/img/GUI/pen.png",
+        "res/img/GUI/randomPen.png",
+        "res/img/GUI/patternBrush.png",
+        "res/img/GUI/eraser.png",
+        "res/img/GUI/fillShape.png",
+        "res/img/GUI/selectArea.png",
+        "res/img/GUI/arrow.png",
+        "res/img/GUI/entity.png",
+        "res/img/GUI/object.png",
+        "res/img/GUI/hand.png",
+        "res/img/GUI/zoom.png"
+    };
+    // Define some metrics for laying out buttons
     constexpr QSize btnsize(30, 30); // Button size
     constexpr int   padding = 2;     // Padding around buttons
     constexpr int   voffset = 44;    // Vertical offset from where buttons are laid out
     constexpr int   sep = 10;        // Separator space between button cagegories
+    // Define the tool categories
     auto getCateg = [](size_t index) {
         if (index < static_cast<size_t>(Tool::Arrow))
             return 0;
@@ -35,7 +33,7 @@ ToolbarWidget::ToolbarWidget(QWidget *parent_) :
         else
             return 2;
     };
-
+    // Create a button for each tool
     for (size_t i = 0; i < TOOL_COUNT; ++i) {
         QPushButton *&b = m_buttons[i];
         b = new QPushButton(QIcon(images[i]), "", this);
